6bfe9f9b6c912b3fbe31e5f762f0e8ae00a08078
unknown <dave.gonzalez1992@outlook.com>
2025-03-02 17:39:57 -0500

    Creating UserController, UserService, IUserRepository

--- src/main/java/com/api/crud/repositories/IUserRepository.java
+++ src/main/java/com/api/crud/repositories/IUserRepository.java
@@ -1,4 +1,13 @@
 package com.api.crud.repositories;
 
-public interface IUserRepository {
+
+import com.api.crud.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
+
 }
